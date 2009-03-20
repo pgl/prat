8fb53a188061227535736e7d0e33426de040727a
sylvanaar <sylvanaar@d67e15e8-83c1-11de-a568-698954c64862>
2009-03-20 19:15:31 +0000

    Tagging as 3.1.0b1

---
+++ tags/3.1.0b1/modules/ChatFrames.lua
@@ -0,0 +1,188 @@
+﻿---------------------------------------------------------------------------------
+--
+-- Prat - A framework for World of Warcraft chat mods
+--
+-- Copyright (C) 2006-2007  Prat Development Team
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to:
+--
+-- Free Software Foundation, Inc., 
+-- 51 Franklin Street, Fifth Floor, 
+-- Boston, MA  02110-1301, USA.
+--
+--
+-------------------------------------------------------------------------------
+
+
+
+--[[
+Name: PratChatFrames
+Revision: $Revision: 80703 $
+Author(s): Curney (asml8ed@gmail.com)
+           Sylvanaar (sylvanaar@mindspring.com)
+Website: http://www.wowace.com/files/index.php?path=Prat/
+Documentation: http://www.wowace.com/wiki/Prat/Integrated_Modules#ChatFrames
+Subversion: http://svn.wowace.com/wowace/trunk/Prat/
+Discussions: http://groups.google.com/group/wow-prat
+Issues and feature requests: http://code.google.com/p/prat/issues/list
+Description: Module for Prat that adds options for changing chat window parameters.
+Dependencies: Prat
+]]
+
+Prat:AddModuleToLoad(function() 
+
+local Prat = Prat
+
+local PRAT_MODULE = Prat:RequestModuleName("Frames")
+
+if PRAT_MODULE == nil then 
+    return 
+end
+
+local L = Prat.GetLocalizer({})
+
+L:AddLocale("enUS", {
+    ["Frames"] = true,
+    ["Chat window frame parameter options"] = true,
+    ["minchatwidth_name"] = "Set Minimum Width",
+    ["minchatwidth_desc"] = "Sets the minimum width for all chat windows.",
+    ["maxchatwidth_name"] = "Set Maximum Width",
+    ["maxchatwidth_desc"] = "Sets the maximum width for all chat windows.",
+    ["minchatheight_name"] = "Set Minimum Height",
+    ["minchatheight_desc"] = "Sets the minimum height for all chat windows.",
+    ["maxchatheight_name"] = "Set Maximum Height",
+    ["maxchatheight_desc"] = "Sets the maximum height for all chat windows.",
+    ["mainchatonload_name"] = "Force Main Chat Frame On Load",
+    ["mainchatonload_desc"] = "Automatically select the first chat frame and make it active on load.",
+	["framealpha_name"] = "Set Chatframe Alpha",
+	["framealpha_desc"] = "Conrols the transparency of the chatframe when you hover over it with your mouse.",
+} )
+
+
+local mod = Prat:NewModule(PRAT_MODULE)
+
+
+Prat:SetModuleDefaults(mod.name, {
+	profile = {
+        on = true,
+        minchatwidth = 160,
+        minchatwidthdefault = 160,
+        maxchatwidth = 800,
+        maxchatwidthdefault = 800,
+        minchatheight = 120,
+        minchatheightdefault = 120,
+        maxchatheight = 600,
+        maxchatheightdefault = 600,
+        mainchatonload = true,
+        framealpha = DEFAULT_CHATFRAME_ALPHA
+	}
+})
+
+do
+	local frameoption = {
+		name = function(info) return L[info[#info].."_name"] end,
+		desc = function(info) return L[info[#info].."_desc"] end,
+		type="range", min=25, max=1024, step=1
+	}
+	
+	Prat:SetModuleOptions(mod.name, {
+	        name = L["Frames"],
+	        desc = L["Chat window frame parameter options"],
+	        type = "group",
+	        args = {
+	    		minchatwidth = frameoption,
+			    maxchatwidth = frameoption,
+			    minchatheight = frameoption,
+			    maxchatheight = frameoption,
+			    framealpha = {
+					name = L["framealpha_name"],
+					desc = L["framealpha_desc"],
+					type="range", min=0.0, max=1.0, step=.01, order=190,
+				},
+	--		    mainchatonload = 200,
+	        }
+	    }
+	)
+end
+
+
+--[[------------------------------------------------
+    Module Event Functions
+------------------------------------------------]]--
+
+Prat:SetModuleInit(mod, function(self) mod:GetDefaults() end)
+
+function mod:OnModuleEnable()
+    self:ConfigureAllChatFrames(true)
+end
+
+function mod:OnModuleDisable()
+    self:ConfigureAllChatFrames(false)
+end
+
+--[[------------------------------------------------
+    Core Functions
+------------------------------------------------]]--
+
+-- make ChatFrame1 the selected chat frame
+function mod:AceEvent_FullyInitialized()
+    if self.db.profile.mainchatonload then
+        FCF_SelectDockFrame(ChatFrame1)
+    end
+end
+
+-- set parameters for each chatframe
+function mod:ConfigureAllChatFrames(enabled)
+    for _,v in pairs(Prat.Frames) do
+        self:SetParameters(v, enabled)
+    end
+    
+    DEFAULT_CHATFRAME_ALPHA = self.db.profile.framealpha
+end
+
+-- get the defaults for chat frame1 max/min width/height for use when disabling the module
+function mod:GetDefaults()
+    local cf = _G["ChatFrame1"]
+	local prof = self.db.profile
+
+    local minwidthdefault, minheightdefault = cf:GetMinResize()
+    local maxwidthdefault, maxheightdefault = cf:GetMaxResize()
+
+    prof.minchatwidthdefault = minwidthdefault
+    prof.maxchatwidthdefault = maxwidthdefault
+    prof.minchatheightdefault = minheightdefault
+    prof.maxchatheightdefault = maxheightdefault
+
+    prof.initialized = true
+end
+
+-- set the max/min width/height for a chatframe
+function mod:SetParameters(cf, enabled)
+	local prof = self.db.profile
+    if enabled then
+        cf:SetMinResize(prof.minchatwidth, prof.minchatheight)
+        cf:SetMaxResize(prof.maxchatwidth, prof.maxchatheight)
+    else
+        cf:SetMinResize(prof.minchatwidthdefault, prof.minchatheightdefault)
+        cf:SetMaxResize(prof.maxchatwidthdefault, prof.maxchatheightdefault)
+    end
+end
+
+
+function mod:OnValueChanged()
+	self:ConfigureAllChatFrames(true)
+end
+
+  return
+end ) -- Prat:AddModuleToLoad
---
+++ tags/3.1.0b1/modules/Paragraph.lua
@@ -0,0 +1,204 @@
+﻿---------------------------------------------------------------------------------
+--
+-- Prat - A framework for World of Warcraft chat mods
+--
+-- Copyright (C) 2006-2007  Prat Development Team
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to:
+--
+-- Free Software Foundation, Inc., 
+-- 51 Franklin Street, Fifth Floor, 
+-- Boston, MA  02110-1301, USA.
+--
+--
+-------------------------------------------------------------------------------
+
+
+
+--[[
+Name: PratParagraph
+Revision: $Revision: 80705 $
+Author(s): Curney (asml8ed@gmail.com)
+Thanks to: Wubin for helping me understand variables better
+Website: http://www.wowace.com/files/index.php?path=Prat/
+Documentation: http://www.wowace.com/wiki/Prat/Integrated_Modules#Paragraph
+SVN: http://svn.wowace.com/wowace/trunk/Prat/
+Description: Module for Prat that gives options for paragraphs in chat windows (defaults=left-aligned text, linespacing=0).
+Dependencies: Prat
+]]
+
+Prat:AddModuleToLoad(function() 
+
+-- Get Utility Libraries
+local PRAT_MODULE = Prat:RequestModuleName("Paragraph")
+
+if PRAT_MODULE == nil then 
+    return 
+end
+
+local L = Prat:GetLocalizer({})
+
+--@debug@
+L:AddLocale("enUS", {
+    ["Paragraph"] = true,
+    ["Chat window paragraph options."] = true,
+    ["justification_name"] = "Set Alignment",
+    ["justification_desc"] = "Set horizontal alignment for each chat window",
+    ["Line Spacing"] = true,
+    ["Set the line spacing for all chat windows."] = true,
+    ["adjustlinks_name"] = "Fix placement of player/item links",
+    ["adjustlinks_desc"] = "Adjust links to restore clickability on centered or right-aligned text.",    
+	["Center"] = true,
+	["Right"] = true,
+	["Left"] = true,
+})
+--@end-debug@
+
+-- These Localizations are auto-generated. To help with localization
+-- please go to http://www.wowace.com/projects/prat-3-0/localization/
+
+
+--[===[@non-debug@
+L:AddLocale("enUS", 
+--@localization(locale="enUS", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+L:AddLocale("frFR",  
+--@localization(locale="frFR", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+L:AddLocale("deDE", 
+--@localization(locale="deDE", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+L:AddLocale("koKR",  
+--@localization(locale="koKR", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+L:AddLocale("esMX",  
+--@localization(locale="esMX", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+L:AddLocale("ruRU",  
+--@localization(locale="ruRU", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+L:AddLocale("zhCN",  
+--@localization(locale="zhCN", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+L:AddLocale("esES",  
+--@localization(locale="esES", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+L:AddLocale("zhTW",  
+--@localization(locale="zhTW", format="lua_table", same-key-is-true=true, namespace="Paragraph")@
+)
+--@end-non-debug@]===]
+
+
+
+
+----[[
+--	Chinese Local : CWDG Translation Team 昏睡墨鱼 (Thomas Mo)
+--	CWDG site: http://Cwowaddon.com
+--	$Rev: 80705 $
+--]]
+--
+
+--
+
+--
+
+--
+
+--
+
+-- 
+
+--
+
+-- create prat module
+local module = Prat:NewModule(PRAT_MODULE)
+
+Prat:SetModuleDefaults(module, {
+	profile = {
+	    on = false,
+	    justification = { ["*"] = "LEFT" },
+	    adjustlinks = false,
+	}
+} )
+
+do
+	local justifyoption = {
+			name = function(info) return Prat.HookedFrameList[info[#info]] or "" end,
+			desc = L["justification_name"],
+			type="select",
+			get = function(info) return info.handler.db.profile.justification[info[#info]] end,
+			set = function(info, v) info.handler.db.profile.justification[info[#info]] = v info.handler:OnValueChanged(info, v) end,
+			values = { ["RIGHT"] = L["Right"], ["CENTER"] = L["Center"], ["LEFT"] =  L["Left"]},
+			hidden = function(info) return Prat.HookedFrameList[info[#info]] == nil end,
+		}
+	
+	Prat:SetModuleOptions(module, {
+	        name = L["Paragraph"],
+	        desc = L["Chat window paragraph options."],
+	        type = "group",
+	        args = {
+	            justification = {
+	                name = L["justification_name"],
+	                desc = L["justification_desc"],
+	                type = "group",
+					inline = true,
+					args = {
+						ChatFrame1 = justifyoption,
+						ChatFrame2 = justifyoption,
+						ChatFrame3 = justifyoption,
+						ChatFrame4 = justifyoption,
+						ChatFrame5 = justifyoption,
+						ChatFrame6 = justifyoption,
+						ChatFrame7 = justifyoption,
+					}
+				},
+
+				info = {
+					name = "Note: Playerlinks, itemlinks, and any other link type will not work when justification is set to anything other than 'Left'",
+					type = "description",
+					order = 110
+				}
+	        }
+	    }
+	)
+end
+
+--[[------------------------------------------------
+    Module Event Functions
+------------------------------------------------]]--
+
+function module:OnModuleEnable()
+    self:ConfigureAllChatFrames(true)
+end
+
+function module:OnModuleDisable()
+    self:ConfigureAllChatFrames(false)
+end
+
+function module:OnValueChanged()
+	self:ConfigureAllChatFrames(true)
+end
+
+--[[------------------------------------------------
+	Core Functions
+------------------------------------------------]]--
+function module:ConfigureAllChatFrames(enable)
+	local prof = self.db.profile
+	for k,v in pairs(Prat.Frames) do
+        v:SetJustifyH(enable and prof.justification[k] or "LEFT")
+	end
+end
+
+  return
+end ) -- Prat:AddModuleToLoad
