c906535c244263ceb5a1ccdc0259526ab7d57bb0
sylvanaar <sylvanaar@d67e15e8-83c1-11de-a568-698954c64862>
2009-07-09 15:22:01 +0000

    Tagging as 3.2.1 - so ppl who aren't set to get betas will work on the ptr without difficultycolor errors

---
+++ tags/3.2.1/modules/ChatTabs.lua
@@ -0,0 +1,337 @@
+﻿---------------------------------------------------------------------------------
+--
+-- Prat - A framework for World of Warcraft chat mods
+--
+-- Copyright (C) 2006-2007  Prat Development Team
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to:
+--
+-- Free Software Foundation, Inc., 
+-- 51 Franklin Street, Fifth Floor, 
+-- Boston, MA  02110-1301, USA.
+--
+--
+-------------------------------------------------------------------------------
+
+
+
+--[[
+Name: PratChatTabs
+Revision: $Revision: 80499 $
+Author(s): Sylvanaar (sylvanaar@mindspring.com)
+Website: http://www.wowace.com/files/index.php?path=Prat/
+Documentation: http://www.wowace.com/wiki/Prat/Integrated_Modules#Tabs
+Subversion: http://svn.wowace.com/wowace/trunk/Prat/
+Discussions: http://groups.google.com/group/wow-prat
+Issues and feature requests: http://code.google.com/p/prat/issues/list
+Description: Module for Prat that adds chat window tab options (default = hidden).
+Dependencies: Prat
+]]
+
+Prat:AddModuleToLoad(function() 
+
+local PRAT_MODULE = Prat:RequestModuleName("ChatTabs")
+
+local dbg = function() end
+--@debug@
+dbg = Prat.PrintLiteral
+--@end-debug@
+
+if PRAT_MODULE == nil then 
+    return 
+end
+
+local L = Prat:GetLocalizer({})
+
+--@debug@
+L:AddLocale("enUS", {
+    ["Tabs"] = true,
+    ["Chat window tab options."] = true,
+    ["Set Display Mode"] = true,
+    ["Set tab display mode for each chat window."] = "Set tab display mode for each chat window. Checked (on), Unchecked (off), Greyed Check (default)",
+    ["Set ChatFrame%d Display Mode"] = true,
+    ["Set tab display to always, hidden or the Blizzard default."] = true,
+    ["Active Alpha"] = true,
+    ["Sets alpha of chat tab for active chat frame."] = true,
+    ["Not Active Alpha"] = true,
+    ["Sets alpha of chat tab for not active chat frame."] = true,
+    ["All"] = true,
+    ["Individual"] = true,
+    ["Always"] = true,
+    ["Hidden"] = true,
+    ["Default"] = true,
+    ["disableflash_name"] =  "Disable Flashing",
+    ["disableflash_desc"] =  "Disable flashing of the chat tabs.",
+    ["preventdrag_name"] =  "Prevent Dragging",
+    ["preventdrag_desc"] =  "Prevent dragging chat tabs with mouse",
+})
+--@end-debug@
+
+-- These Localizations are auto-generated. To help with localization
+-- please go to http://www.wowace.com/projects/prat-3-0/localization/
+
+
+--[===[@non-debug@
+L:AddLocale("enUS", 
+--@localization(locale="enUS", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+L:AddLocale("frFR",  
+--@localization(locale="frFR", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+L:AddLocale("deDE", 
+--@localization(locale="deDE", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+L:AddLocale("koKR",  
+--@localization(locale="koKR", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+L:AddLocale("esMX",  
+--@localization(locale="esMX", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+L:AddLocale("ruRU",  
+--@localization(locale="ruRU", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+L:AddLocale("zhCN",  
+--@localization(locale="zhCN", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+L:AddLocale("esES",  
+--@localization(locale="esES", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+L:AddLocale("zhTW",  
+--@localization(locale="zhTW", format="lua_table", same-key-is-true=true, namespace="ChatTabs")@
+)
+--@end-non-debug@]===]
+
+
+local module = Prat:NewModule(PRAT_MODULE, "AceHook-3.0")
+
+Prat:SetModuleDefaults(module.name, {
+	profile = {
+        on = false,
+        displaymode = { },
+        disableflash = false,
+        notactivealpha = 0.5,
+        activealpha = 1,
+		preventdrag = false,
+	}
+} )
+
+--module.toggleOptions = { sep115_sep = 115, disableflash = 120, preventdrag = 125 }
+
+Prat:SetModuleOptions(module.name, {
+        name = L["Tabs"],
+        desc = L["Chat window tab options."],
+        type = "group",
+        args = {
+            displaymode = {
+                name = L["Set Display Mode"],
+                desc = L["Set tab display mode for each chat window."],
+                type = "multiselect",
+				tristate = true,
+                order = 110,
+				values = Prat.FrameList,
+				get = "GetSubValue",
+				set = "SetSubValue",
+			},
+			disableflash = {
+				name = L["disableflash_name"],
+				desc = L["disableflash_desc"],
+				type = "toggle",
+				order = 120
+			},
+			preventdrag = {
+				name = L["preventdrag_name"],
+				desc = L["preventdrag_desc"],
+				type = "toggle",
+				order = 120
+			},
+            activealpha = {
+                name = L["Active Alpha"],
+                desc = L["Sets alpha of chat tab for active chat frame."],
+                type = "range",
+                order = 130,
+                min = 0.1,
+                max = 1,
+                step = 0.1,
+            },
+            notactivealpha = {
+                name = L["Not Active Alpha"],
+                desc = L["Sets alpha of chat tab for not active chat frame."],
+                type = "range",
+                order = 140,
+                min = 0.1,
+                max = 1,
+                step = 0.1,
+            },
+        }
+    }
+)
+
+--local tabmode = { ["true"] = "ALWAYS", ["false"] = "HIDDEN", ["nil"] = "DEFAULT" }
+
+--[[------------------------------------------------
+    Module Event Functions
+------------------------------------------------]]--
+
+-- things to do when the module is enabled
+function module:OnModuleEnable()
+    self:SecureHook("FCF_FlashTab")
+
+    self:HookedMode(true)
+
+    self:UpdateAllTabs()
+end
+
+-- things to do when the module is enabled
+function module:OnModuleDisable()
+    self:RemoveHooks()
+end
+
+--[[------------------------------------------------
+    Core Functions
+------------------------------------------------]]--
+
+
+function module:HookedMode(hooked)
+    if hooked then
+        self:RawHook("FCF_Close", true)
+        self:InstallHooks()
+        Prat.UnregisterChatEvent(self, Prat.Events.POST_ADDMESSAGE) 
+    else
+        self:RemoveHooks()
+        Prat.RegisterChatEvent(self, Prat.Events.POST_ADDMESSAGE)   
+    end
+end
+
+
+local needToHook = {}
+
+function module:InstallHooks()
+    for k, v in pairs(Prat.Frames) do
+        local cftab = _G[k.."Tab"]
+        self:HookScript(cftab, "OnShow", "OnTabShow")
+        if cftab:IsShown() then
+            self:HookScript(cftab, "OnHide", "OnTabHide")
+            needToHook[cftab] = nil
+        else
+            needToHook[cftab] = true
+        end
+		self:HookScript(cftab,"OnDragStart", "OnTabDragStart")
+    end
+end
+
+function module:RemoveHooks()
+	for k, v in pairs(Prat.Frames) do
+        local cftab = getglobal(k.."Tab")
+        cftab:SetScript("OnShow", function() return end)
+        cftab:SetScript("OnHide", function() return end)
+    end
+    -- unhook functions
+    self:UnhookAll()
+end
+
+function module:OnValueChanged(info, b)
+--	if info[#info]:find("alpha") then
+--		
+--		return
+--	end
+	
+	self:UpdateAllTabs()
+end
+
+function module:OnSubValueChanged(info, b)
+	self:UpdateAllTabs()
+end
+
+function module:Prat_PostAddMessage(info, message, frame, event, text, r, g, b, id)
+    frame.tellTimer = GetTime() + CHAT_TELL_ALERT_TIME;
+    FCF_FlashTab(frame)
+end
+
+function module:UpdateAllTabs()
+    for k,v in pairs(Prat.Frames) do 
+        if FCF_IsValidChatFrame(v) then
+            local chatTab = _G[k.."Tab"]
+            chatTab:Show()
+            chatTab:Hide()
+            FloatingChatFrame_Update(v:GetID()) 
+        end
+    end
+end
+
+function module:OnTabShow(tab, ...)
+    if needToHook[tab] then
+        self:HookScript(tab, "OnHide", "OnTabHide")
+        needToHook[tab] = nil
+    end
+
+    if self.db.profile.displaymode["ChatFrame"..tab:GetID()] == false then
+       tab:Hide()
+    end  
+end
+
+function module:OnTabHide(tab, ...)    
+    local p = self.db.profile
+    local i = tab:GetID()   
+    
+	if self.db.profile.displaymode["ChatFrame"..tab:GetID()] == true then
+        tab:Show()
+        if SELECTED_CHAT_FRAME:GetID() == i then
+            tab:SetAlpha(p.activealpha)
+        else
+            tab:SetAlpha(p.notactivealpha)
+        end
+    end      
+end
+
+function module:OnTabDragStart(this, ...)  
+    local p = self.db.profile
+    
+    if p.preventdrag and p.on then return end
+    
+    self.hooks[this].OnDragStart(this, ...)
+end
+
+
+
+function module:FCF_FlashTab(this)
+    dbg("FCF_FlashTab", this)
+
+    local i = this:GetName()
+    local p = self.db.profile
+    
+    if p.disableflash or p.displaymode[i] == false then 
+        dbg(Prat.CURRENT_MSG)
+       -- _G[i.."TabText"]:SetTextColor(1,0,0)        
+--        print ChatFrame1TabText:SetTextColor(1,0,0)  
+        UIFrameFlashStop(getglobal(i.."TabFlash"))
+   
+        
+    end
+end
+
+function module:FCF_Close(frame, fallback)
+    local tab = _G[frame:GetName().."Tab"]
+
+   -- print(frame, fallback, tab)
+    if tab then
+        self:Unhook(tab, "OnHide")
+        needToHook[tab] = true
+    end
+
+    self.hooks.FCF_Close(frame, fallback)
+end
+
+
+  return
+end ) -- Prat:AddModuleToLoad
---
+++ tags/3.2.1/modules/Scroll.lua
@@ -0,0 +1,320 @@
+﻿---------------------------------------------------------------------------------
+--
+-- Prat - A framework for World of Warcraft chat mods
+--
+-- Copyright (C) 2006-2007  Prat Development Team
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to:
+--
+-- Free Software Foundation, Inc., 
+-- 51 Franklin Street, Fifth Floor, 
+-- Boston, MA  02110-1301, USA.
+--
+--
+-------------------------------------------------------------------------------
+
+
+
+--[[
+Name: module
+Revision: $Revision: 82149 $
+Author(s): Curney (asml8ed@gmail.com)
+           Krtek (krtek4@gmail.com)
+Inspired by: idChat2_PlayerNames by Industrial
+Website: http://www.wowace.com/files/index.php?path=Prat/
+Documentation: http://www.wowace.com/wiki/Prat/Integrated_Modules#Scroll
+SVN: http://svn.wowace.com/wowace/trunk/Prat/
+Description: Module for Prat that enables mousewheel scrolling and TheDownLow for chat windows (default=on).
+Dependencies: Prat
+]]
+
+Prat:AddModuleToLoad(function() 
+
+local PRAT_MODULE = Prat:RequestModuleName("Scroll")
+
+if PRAT_MODULE == nil then 
+    return 
+end
+
+local L = Prat:GetLocalizer({})
+
+--@debug@
+L:AddLocale("enUS", {
+    ["Scroll"] = true,
+    ["Chat window scrolling options."] = true,
+    ["mousewheel_name"] = "Enable MouseWheel",
+    ["mousewheel_desc"] = "Toggle mousewheel support for each chat window.",
+    ["Set MouseWheel Speed"] = true,
+    ["Set number of lines mousewheel will scroll."] = true,
+    ["Set Ctrl+MouseWheel Speed"] = true,
+    ["Set number of lines mousewheel will scroll when ctrl is pressed."] = true,
+    ["lowdown_name"] = "Enable TheLowDown",
+    ["lowdown_desc"] = "Toggle auto jumping to the bottom for each chat window.",
+    ["Set TheLowDown Delay"] = true,
+    ["Set time to wait before jumping to the bottom of chat windows."] = true,
+	["Text scroll direction"] = true,
+	["Control whether text is added to the frame at the top or the bottom."] = true,
+	["Top"] = "Top to bottom",
+	["Bottom"] = "Bottom to top",
+})
+--@end-debug@
+
+-- These Localizations are auto-generated. To help with localization
+-- please go to http://www.wowace.com/projects/prat-3-0/localization/
+
+
+--[===[@non-debug@
+L:AddLocale("enUS", 
+--@localization(locale="enUS", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+L:AddLocale("frFR",  
+--@localization(locale="frFR", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+L:AddLocale("deDE", 
+--@localization(locale="deDE", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+L:AddLocale("koKR",  
+--@localization(locale="koKR", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+L:AddLocale("esMX",  
+--@localization(locale="esMX", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+L:AddLocale("ruRU",  
+--@localization(locale="ruRU", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+L:AddLocale("zhCN",  
+--@localization(locale="zhCN", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+L:AddLocale("esES",  
+--@localization(locale="esES", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+L:AddLocale("zhTW",  
+--@localization(locale="zhTW", format="lua_table", same-key-is-true=true, namespace="Scroll")@
+)
+--@end-non-debug@]===]
+
+
+
+
+----[[
+--	Chinese Local : CWDG Translation Team 昏睡墨鱼 (Thomas Mo)
+--	CWDG site: http://Cwowaddon.com
+--	$Rev: 82149 $
+--]]
+--
+
+--
+
+--
+
+--
+
+-- 
+
+--
+
+
+
+local module = Prat:NewModule(PRAT_MODULE, "AceHook-3.0")
+
+
+Prat:SetModuleDefaults(module.name, {
+	profile = {
+        on = true,
+        mousewheel = { ["*"] = true },
+        normscrollspeed = 1,
+        ctrlscrollspeed = 3,
+        lowdown = { ["*"] = true },
+        lowdowndelay = 20,
+		scrolldirection = "BOTTOM"     
+	}
+} )
+
+
+---- build the options menu using prat templates
+--module.toggleOptions = {
+--    mousewheel_handler = {},
+--    sep135_sep = 135,
+--    lowdown_handler = {}
+--}
+
+
+Prat:SetModuleOptions(module.name, {
+    name = L["Scroll"],
+    desc = L["Chat window scrolling options."],
+    type = "group",
+    args = {
+		 mousewheel = {
+			name = L["mousewheel_name"],
+			desc = L["mousewheel_desc"],
+			type = "multiselect",
+			order = 110,
+			values = Prat.HookedFrameList,
+			get = "GetSubValue",
+			set = "SetSubValue"
+		 },
+         normscrollspeed = {
+            name = L["Set MouseWheel Speed"],
+            desc = L["Set number of lines mousewheel will scroll."],
+            type = "range",
+            order = 120,
+            min = 1,
+            max = 21,
+            step = 1,
+        },
+		scrolldirection = {
+			type = "select", 
+            name = L["Text scroll direction"],
+            desc = L["Control whether text is added to the frame at the top or the bottom."],
+			values = { ["TOP"] = L["Top"], ["BOTTOM"] = L["Bottom"] },
+		},
+        ctrlscrollspeed = {
+            name = L["Set Ctrl+MouseWheel Speed"],
+            desc = L["Set number of lines mousewheel will scroll when ctrl is pressed."],
+            type = "range",
+            order = 130,
+            min = 3,
+            max = 21,
+            step = 3,
+        },
+--		lowdown = {
+--			name = L["lowdown_name"],
+--			desc = L["lowdown_desc"],
+--			type = "multiselect",
+--			order = 110,
+--			values = Prat.HookedFrameList,
+--			get = "GetSubValue",
+--			set = "SetSubValue"
+--		},		
+--        lowdowndelay = {
+--            name = L["Set TheLowDown Delay"],
+--            desc = L["Set time to wait before jumping to the bottom of chat windows."],
+--            type = "range",
+--            order = 220,
+--            min = 1,
+--            max = 60,
+--            step = 1,
+--        },
+    }
+})
+
+module.OnSubValueChanged = module.ConfigureAllFrames
+
+
+--[[------------------------------------------------
+    Module Event Functions
+------------------------------------------------]]--
+
+-- things to do when the module is enabled
+function module:OnModuleEnable()
+	self:ConfigureAllFrames()	
+end
+
+
+
+-- things to do when the module is disabled
+function module:OnModuleDisable()
+    for k, v in pairs(Prat.Frames) do
+        self:MouseWheel(v,false)
+--       	self:LowDown(v,false)
+    end
+
+	self:SetScrollDirection("BOTTOM")
+end
+
+--[[------------------------------------------------
+    Core Functions
+------------------------------------------------]]--
+
+function module:ConfigureAllFrames()
+    for k, v in pairs(Prat.Frames) do
+        self:MouseWheel(v, self.db.profile.mousewheel[k])
+--       	self:LowDown(v, self.db.profile.lowdown[k])
+    end
+
+	self:SetScrollDirection(self.db.profile.scrolldirection)
+end
+
+do
+	local function scrollFrame(cf, up)
+		if IsShiftKeyDown() then
+	        if up then cf:ScrollToTop() else cf:ScrollToBottom() end
+		else
+		    if IsControlKeyDown() then
+		        for i = 1,module.db.profile.ctrlscrollspeed do
+		            if up then cf:ScrollUp() else cf:ScrollDown() end
+		        end
+		    else
+		        for i = 1,module.db.profile.normscrollspeed do
+		            if up then cf:ScrollUp() else cf:ScrollDown() end
+		        end
+		    end
+		end
+	end
+	
+	function module:MouseWheel(cf, enabled)
+	    if enabled then
+	        cf:SetScript("OnMouseWheel", function(cf, arg1) scrollFrame(cf, arg1 > 0) end)
+	        cf:EnableMouseWheel(true)
+	    else
+	        cf:SetScript("OnMouseWheel", nil)
+	        cf:EnableMouseWheel(false)
+	    end
+	end
+end
+
+--function module:LowDown(cf, enabled)
+--	local name = cf:GetName()
+--	local funcs = {"ScrollUp", "ScrollDown", "ScrollToTop", "PageUp", "PageDown"}
+--
+--    if enabled then
+--		for _,func in ipairs(funcs) do
+--			local f = function(cf)
+--				if self:IsEventScheduled(name.."DownTimeout") then self:CancelScheduledEvent(name.."DownTimeout") end
+--				self:ScheduleEvent(name.."DownTimeout", self.ResetFrame, self.db.profile.lowdowndelay, self, cf)
+--			end
+--			self:SecureHook(cf, func, f)
+--		end
+--	else
+--		for _,func in ipairs(funcs) do
+--			if self:IsHooked(cf, func) then self:Unhook(cf, func) end
+--		end
+--	end
+--end
+
+function module:ResetFrame(cf)
+	if not cf:AtBottom() then
+		cf:ScrollToBottom()
+	end
+end
+
+function module:SetScrollDirection(direction)
+    for k, v in pairs(Prat.HookedFrames) do
+		self:ScrollDirection(v, direction)
+    end
+
+	self.db.profile.scrolldirection = direction
+end
+
+function module:ScrollDirection(cf, direction)
+	if cf:GetInsertMode() ~= direction then 
+		cf:SetMaxLines(cf:GetMaxLines())
+		cf:SetInsertMode(direction)
+	end
+end
+
+
+
+  return
+end ) -- Prat:AddModuleToLoad
